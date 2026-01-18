6cbe5e0bb916017e36d90bd50db6ef4ea8683c6b
Bhuwan Adhikari <bhuwanadhikari7788@gmail.com>
2026-01-18 12:56:55 +0100

    update

--- readme.sh
+++
@@ -1,4 +0,0 @@
-# 📊 GitHub Stats:
-![](https://github-readme-stats.vercel.app/api?username=bhuwanadhikari&theme=onedark&hide_border=false&include_all_commits=true&count_private=true)<br/>
-![](https://nirzak-streak-stats.vercel.app/?user=bhuwanadhikari&theme=onedark&hide_border=false)<br/>
-![](https://github-readme-stats.vercel.app/api/top-langs/?username=bhuwanadhikari&theme=onedark&hide_border=false&include_all_commits=true&count_private=true&layout=compact)
